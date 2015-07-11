8bc2475b8ea931bc6799b27f9f5b3ef1de861425
admin <admin@notebook-20>
2015-07-11 19:07:02 -0300

    Commit Inicial

--- dbxTE.c
+++ dbxTE.c
@@ -3,6 +3,20 @@
  * Author: admin
  *
  * Created on July 11, 2015, 6:48 PM
+ * 
+ * usage: dbdump [-hcdfa] dbf_file table_name
+
+    Dump dBase III .dbf files into SQL commands. Records marked for
+    delete are left out from the dump.
+
+	-h only the header of the .dbf file
+	-c only the structure of the table (CREATE commands)
+	-d only data records of the table (INSERT commands)
+	-f list the field names in INSERT commands
+	-a dump into text file suitable to import by MS Access
+	default: structure and records without field names
+
+ * 
  */
 
 #include <stdio.h>
