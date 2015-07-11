fcc3d46ea51b0ec94d53da1d6d16e9dd986db3fb
admin <admin@notebook-20>
2015-07-11 19:36:01 -0300

    Commit v0

--- dbxTE.c
+++ dbxTE.c
@@ -1,6 +1,6 @@
 /* 
  * File:   dbxTE.c
- * Author: admin
+ * Author: Andre Almeida
  *
  * Created on July 11, 2015, 6:48 PM
  * 
@@ -21,7 +21,45 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include "xbase.h"
 
+struct dbf_header hdr;
+struct field_descriptor fld[MAX_NO_FIELDS];
+int no_fields;
+int no_records;
+short rec_len;
+char buf[MAX_REC_LEN];
+char usage[] = "usage: dbdump [-hcdfa] dbf_file table_name\nDump dBase III .dbf files into SQL commands. Records marked for\ndelete are left out from the dump.\n-h only the header of the .dbf file\n-c only the structure of the table (CREATE commands)\n-d only data records of the table (INSERT commands)\n-f list the field names in INSERT commands\n-a dump into text file suitable to import by MS Access\ndefault: structure and records without field names";
+
+
+void Error(char *message, int error_code) {
+    if (message != NULL) {
+	fprintf(stderr, "%s\n", message);
+    }
+    if (error_code >= 0) {
+	fprintf(stderr, "%s\n", strerror(error_code));
+    }
+    exit(1);
+}
+
+
+void Warning(char *message)
+{
+    fprintf(stderr, "%s\n", message);
+    fflush(stderr);
+}
+
+
+int big_endian()
+{
+int x = 1;
+
+    if(*(char *)&x == 1) {
+	return 0;
+    } else {
+	return 1;
+    }
+}
 /*
  * 
  */
--- xbase.h
+++ xbase.h
@@ -1,8 +1,11 @@
 /* 
  * File:   xbase.h
- * Author: admin
+ * Author: Andre Almeida
  *
  * Created on July 11, 2015, 6:49 PM
+ * 
+ * Classe derivada de:
+ * 
  */
 
 #ifndef XBASE_H
@@ -13,6 +16,35 @@ extern "C" {
 #endif
 
 
+#define	TERMINATOR	'\r'
+#define	LIVE		' '
+#define	DELETED		'*'
+#define MAX_REC_LEN	2048
+#define MAX_NO_FIELDS	256
+
+// .DBF header
+struct dbf_header {
+    char	version;	// dBASE version number
+    char	year;
+    char	month;
+    char	day;
+    int		no_records;	// number of records in file
+    short	hdr_len;	// length of header
+    short	rec_len;	// number of bytes in record
+    char	misc[20];	// reserved bytes
+};
+
+
+// field descriptor
+#define FIELD_NAME_LENGTH	11
+struct field_descriptor {
+    char	name[FIELD_NAME_LENGTH];
+    char	type;
+    int		address;
+    char	length;
+    char	decimals;
+    char	misc[14];
+};
 
 
 #ifdef	__cplusplus
