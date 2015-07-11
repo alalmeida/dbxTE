210b6f9442f974a571384785c531f57f272ed826
admin <admin@notebook-20>
2015-07-11 19:04:59 -0300

    Commit Inicial

---
+++ dbxTE.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   dbxTE.c
+ * Author: admin
+ *
+ * Created on July 11, 2015, 6:48 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
---
+++ xbase.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   xbase.h
+ * Author: admin
+ *
+ * Created on July 11, 2015, 6:49 PM
+ */
+
+#ifndef XBASE_H
+#define	XBASE_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* XBASE_H */
+
